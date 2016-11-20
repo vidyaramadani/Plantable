357d2ba0e77c7145e432fe5cc6eeebe34ea40aad
Monica <monicabrlpt@gmail.com>
2016-11-19 23:23:16 -1200

    commit

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl501101928/plantable/BuahFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl501101928/plantable/BuahFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -14,11 +15,12 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class BuahFragment extends Fragment {
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    Button btnjeruk;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -58,8 +60,23 @@ public class BuahFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_buah, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_buah, container, false);
+
+        btnjeruk = (Button) view.findViewById(R.id.btnjeruk);
+        btnjeruk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return view;
+    }
+
+    public interface onNameSetListener {
+        void setName(String name);
     }
 
 }

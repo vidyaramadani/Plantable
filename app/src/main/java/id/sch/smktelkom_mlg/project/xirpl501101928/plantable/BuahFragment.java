955d4d1fe7cc3c5bd181f61e30f32989af0e73b7
Monica <monicabrlpt@gmail.com>
2016-11-21 02:28:49 -1200

    commit

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl501101928/plantable/BuahFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl501101928/plantable/BuahFragment.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.project.xirpl501101928.plantable;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +22,7 @@ public class BuahFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
     Button btnjeruk;
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -63,20 +66,46 @@ public class BuahFragment extends Fragment {
 
         // Inflate the layout for this fragment
 
-        View view = inflater.inflate(R.layout.fragment_buah, container, false);
+        return inflater.inflate(R.layout.fragment_buah, container, false);
+    }
 
-        btnjeruk = (Button) view.findViewById(R.id.btnjeruk);
-        btnjeruk.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        getView().findViewById(R.id.btnjeruk).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(getActivity(), JerukActivity.class));
             }
         });
-        return view;
-    }
 
-    public interface onNameSetListener {
-        void setName(String name);
-    }
+        getView().findViewById(R.id.btnsemangka).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), SemangkaActivity.class));
+            }
+        });
 
+        getView().findViewById(R.id.btnpisang).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), PisangActivity.class));
+            }
+        });
+
+        getView().findViewById(R.id.btnlemon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), LemonActivity.class));
+            }
+        });
+
+        getView().findViewById(R.id.btnstrawberry).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), StrawberryActivity.class));
+            }
+        });
+    }
 }
